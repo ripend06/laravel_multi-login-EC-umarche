f1d3ac5476ff356a6f540bb3c2650192aeb13029
ripend06 <makoto6760@gmail.com>
2024-04-06 23:02:48 +0900

    feat:add-slot-with-name_240406(名前付きスロット)

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -20,6 +20,9 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
+        <header>
+            {{ $header }}
+        </header>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,5 @@
 <x-tests.app>
+    <x-slot name="header">ヘッダー2</x-slot>
+
 コンポーネントテスト２
 </x-tests.app>
