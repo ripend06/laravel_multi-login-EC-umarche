22487633fb3f9e8309a090cda5b7ae095605afa0
ripend06 <makoto6760@gmail.com>
2024-04-08 00:00:29 +0900

    feat:add-service-conteainer_240408(サービスコンテナ)

---
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    //
+    public function showServiceContainerTest()
+    {
+        //サービスコンテナ登録
+        app()->bind('lifeCycleTest', function(){
+            return 'ライフサイクルテスト';
+        });
+
+        //サービスコンテナから取り出す
+        $test = app()->make('lifeCycleTest');
+
+
+        //①サービスコンテナなしのパターン
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        //②サービスコンテナapp()ありのパターン
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
+        //サービスコンテナ確認
+        dd($test, app());
+    }
+}
+
+// Message クラス: メッセージを送信するクラス
+class Message
+{
+    // メッセージを送信するメソッド
+    public function send(){
+        echo('メッセージ表示');
+    }
+}
+
+// Sample クラス: Message クラスに依存するクラス
+class Sample
+{
+    // Message インスタンスを格納するプロパティ
+    public $message;
+
+    // ★Message クラスのインスタンスを受け取るコンストラクタ
+    public function __construct(Message $message){
+        $this->message = $message;
+    }
+
+    // Message インスタンスの send() メソッドを実行するメソッド
+    public function run(){
+        $this->message->send();
+    }
+}
