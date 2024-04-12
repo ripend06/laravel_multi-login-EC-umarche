f4d8e73c789d26e7e899622233db44c1446fdba6
ripend06 <makoto6760@gmail.com>
2024-04-13 01:29:28 +0900

    feat:add-resourceContoroller_240413(リソースコントローラー)

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +25,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.welcome');
 });
+
+//リソースコントローラー　Adminで認証していたら表示する
+Route::resource('owners', OwnersController::class)
+->middleware('auth:admin');
+
+
+
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
